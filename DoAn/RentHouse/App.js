f28c3bfbd4bc161e164de2da7afa28b8e27da3f0
daotrananhtuan09102002 <79463278+daotrananhtuan09102002@users.noreply.github.com>
2022-12-14 23:34:22 +0700

    update UI menu

--- DoAn/RentHouse/App.js
+++ DoAn/RentHouse/App.js
@@ -1,6 +1,7 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { app_styles } from './mystyles.js';
 
 
 import Menu from './src/StackComponents/Menu.js'
@@ -22,7 +23,7 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer>  {/*theme={MyTheme}*/}
       <Stack.Navigator>
         <Stack.Screen name="Menu" component={Menu} options={{
           headerShown: false,
@@ -65,11 +66,12 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
+// de lam sau
+// const MyTheme = {
+//   ...DefaultTheme,
+//   colors: {
+//     ...DefaultTheme.colors,
+//     background: app_styles.gray,
+//   },
+// };
+
