e44bab1605ec286f9bb7ac159f127cc0d538b361
Pta Tien <anhtien372002@gmail.com>
2023-01-12 18:37:11 +0700

    prettier module ... ???

--- DoAn/RentHouse/src/StackComponents/RoomDetail.js
+++ DoAn/RentHouse/src/StackComponents/RoomDetail.js
@@ -13,11 +13,14 @@ import { LogBox } from 'react-native';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import IonIcon from 'react-native-vector-icons/Ionicons';
 
-import { fetchRoomDetails, fetchRoomMemberList, fetchRoomBillList } from '../database/actions/roomActions';
+import {
+	fetchRoomDetails,
+	fetchRoomMemberList,
+	fetchRoomBillList,
+} from '../database/actions/roomActions';
 import { deleteCustomer } from '../database/actions/customerActions';
 import { useForceUpdate } from '../utils/utils';
 import { fetchServiceDetails } from '../database/actions/serviceActions';
-
 
 export default function App({ navigation, route }) {
 	const selected_room_id = route.params.selected_room_id;
@@ -35,27 +38,31 @@ export default function App({ navigation, route }) {
 
 	useEffect(() => {
 		const loadRoomDetails = async () => {
-			const roomDetails = await fetchRoomDetails(selected_room_id)
-				.catch((error) => console.log(error));
+			const roomDetails = await fetchRoomDetails(selected_room_id).catch(
+				(error) => console.log(error)
+			);
 			setRoom(roomDetails);
 		};
 
 		const loadMemberList = async () => {
-			const memberList = await fetchRoomMemberList(selected_room_id)
-				.catch((error) => console.log(error));
+			const memberList = await fetchRoomMemberList(selected_room_id).catch(
+				(error) => console.log(error)
+			);
 			setMemberList(memberList);
 			console.log(memberList);
 		};
 
 		const loadBillList = async () => {
-			const billList = await fetchRoomBillList(selected_room_id)
-				.catch((error) => console.log(error));
+			const billList = await fetchRoomBillList(selected_room_id).catch(
+				(error) => console.log(error)
+			);
 			setBillList(billList);
 		};
 
 		const loadService = async (service, setService) => {
-			const fee = await fetchServiceDetails(service)
-				.catch((error) => console.log(error));
+			const fee = await fetchServiceDetails(service).catch((error) =>
+				console.log(error)
+			);
 
 			setService(fee.price);
 		};
@@ -67,7 +74,6 @@ export default function App({ navigation, route }) {
 		loadBillList();
 
 		LogBox.ignoreLogs(['VirtualizedLists should never be nested']);
-
 	}, [isFocused, forceUpdateId]);
 
 	const totalRemained = billList.reduce((res, curr) => {
@@ -75,8 +81,9 @@ export default function App({ navigation, route }) {
 	}, 0);
 
 	const handleDeleteMember = async (memberID) => {
-		await deleteCustomer(memberID, forceUpdate)
-			.catch((error) => console.log(error));
+		await deleteCustomer(memberID, forceUpdate).catch((error) =>
+			console.log(error)
+		);
 	};
 
 	const renderMembers = ({ item }) => {
@@ -86,15 +93,10 @@ export default function App({ navigation, route }) {
 					navigation.navigate('MemberDetail', {
 						memberID: item.id,
 					});
-				}}
-			>
+				}}>
 				<View style={[styles.member, styles.myBackground]}>
 					{/* Member icon */}
-					<FontAwesomeIcon
-						name="user"
-						size={20}
-						style={{ marginRight: 32 }}
-					/>
+					<FontAwesomeIcon name="user" size={20} style={{ marginRight: 32 }} />
 
 					{/* Member name */}
 					<Text>{item.name}</Text>
@@ -104,16 +106,14 @@ export default function App({ navigation, route }) {
 						<TouchableOpacity
 							onPress={() => {
 								handleDeleteMember(item.id);
-							}}
-						>
+							}}>
 							<FontAwesomeIcon
 								name="remove"
 								size={25}
-								style={{color: 'white'}}
+								style={{ color: 'white' }}
 							/>
 						</TouchableOpacity>
 					</View>
-					
 				</View>
 			</TouchableOpacity>
 		);
@@ -126,8 +126,7 @@ export default function App({ navigation, route }) {
 					navigation.navigate('BillDetail', {
 						selected_bill_id: item.id,
 					});
-				}}
-			>
+				}}>
 				<View style={[styles.myBackground, styles.billInfoContainer]}>
 					<View style={styles.billInfo}>
 						{/* Bill month year */}
@@ -153,8 +152,7 @@ export default function App({ navigation, route }) {
 										justifyContent: 'space-between',
 										alignItems: 'center',
 									},
-								]}
-							>
+								]}>
 								<View>
 									<Text>Đã thu:</Text>
 									<Text style={styles.textBold}>
@@ -166,9 +164,7 @@ export default function App({ navigation, route }) {
 							{/* Remained */}
 							<View style={[styles.myBorder, styles.money]}>
 								<Text>Còn lại: </Text>
-								<Text style={styles.textBold}>
-									{item.remained}đ
-								</Text>
+								<Text style={styles.textBold}>{item.remained}đ</Text>
 							</View>
 						</View>
 					</View>
@@ -177,23 +173,16 @@ export default function App({ navigation, route }) {
 						<TouchableOpacity
 							onPress={() => {
 								// Handle delete selected bill
-							}}
-						>
+							}}>
 							<FontAwesomeIcon
 								name="remove"
 								size={25}
-								style={{color: 'white'}}
+								style={{ color: 'white' }}
 							/>
 						</TouchableOpacity>
 					</View>
 				</View>
-
 			</TouchableOpacity>
-
-
-				
-				
-			
 		);
 	};
 
@@ -203,26 +192,18 @@ export default function App({ navigation, route }) {
 			<View style={[styles.header]}>
 				{/* Info and Bill button */}
 				<TouchableOpacity
-					style={[
-						styles.headerBtn,
-						{ borderBottomWidth: mountInfo ? 4 : 0 },
-					]}
+					style={[styles.headerBtn, { borderBottomWidth: mountInfo ? 4 : 0 }]}
 					onPress={() => {
 						if (!mountInfo) setMountInfo(!mountInfo);
-					}}
-				>
+					}}>
 					<Text style={styles.textBold}>THÔNG TIN</Text>
 				</TouchableOpacity>
 
 				<TouchableOpacity
-					style={[
-						styles.headerBtn,
-						{ borderBottomWidth: !mountInfo ? 4 : 0 },
-					]}
+					style={[styles.headerBtn, { borderBottomWidth: !mountInfo ? 4 : 0 }]}
 					onPress={() => {
 						if (mountInfo) setMountInfo(!mountInfo);
-					}}
-				>
+					}}>
 					<Text style={styles.textBold}>LỊCH SỬ HÓA ĐƠN</Text>
 				</TouchableOpacity>
 			</View>
@@ -230,87 +211,54 @@ export default function App({ navigation, route }) {
 			{/* Body */}
 			<ScrollView
 				style={{ width: '100%', marginTop: 72 }}
-				contentContainerStyle={{ flexGrow: 1, alignItems: 'center' }}
-			>
+				contentContainerStyle={{ flexGrow: 1, alignItems: 'center' }}>
 				{/* View info */}
 				{mountInfo && (
 					<View style={styles.infoContainer}>
 						{/* View room details */}
 						<View style={[styles.basicInfo, styles.myBorder]}>
-							<View style={[
-									styles.bodyHeader,
-									{ height: 35, marginBottom: 16 },
-								]}>
+							<View
+								style={[styles.bodyHeader, { height: 35, marginBottom: 16 }]}>
 								<Text>Thông tin cơ bản</Text>
 								<TouchableOpacity
 									onPress={() => {
 										navigation.navigate('EditBasicInfo', {
-											roomID: room.id
-										})
-									}}
-								>
-									<FontAwesomeIcon
-										name="edit"
-										size={25}
-									/>
+											roomID: room.id,
+										});
+									}}>
+									<FontAwesomeIcon name="edit" size={25} />
 								</TouchableOpacity>
 							</View>
 
 							<View style={styles.infoRow}>
 								{/* Room name */}
-								<View
-									style={[
-										styles.rowItem,
-										styles.myBackground,
-									]}
-								>
+								<View style={[styles.rowItem, styles.myBackground]}>
 									<View>
 										<Text>Tên phòng:</Text>
-										<Text style={styles.textBold}>
-											{room.name}
-										</Text>
+										<Text style={styles.textBold}>{room.name}</Text>
 									</View>
 								</View>
 
 								{/* Move in day */}
-								<View
-									style={[
-										styles.rowItem,
-										styles.myBackground,
-									]}
-								>
+								<View style={[styles.rowItem, styles.myBackground]}>
 									<View>
 										<Text>Ngày đến:</Text>
-										<Text style={styles.textBold}>
-											{room.move_in_date}
-										</Text>
+										<Text style={styles.textBold}>{room.move_in_date}</Text>
 									</View>
 								</View>
 							</View>
 
 							<View style={styles.infoRow}>
 								{/* Rental fee */}
-								<View
-									style={[
-										styles.rowItem,
-										styles.myBackground,
-									]}
-								>
+								<View style={[styles.rowItem, styles.myBackground]}>
 									<View>
 										<Text>Giá thuê:</Text>
-										<Text style={styles.textBold}>
-											{room.rental_fee}
-										</Text>
+										<Text style={styles.textBold}>{room.rental_fee}</Text>
 									</View>
 								</View>
 
 								{/* Deposit */}
-								<View
-									style={[
-										styles.rowItem,
-										styles.myBackground,
-									]}
-								>
+								<View style={[styles.rowItem, styles.myBackground]}>
 									<View>
 										<Text>Tiền cọc:</Text>
 										<Text style={styles.textBold}>
@@ -324,11 +272,7 @@ export default function App({ navigation, route }) {
 						{/* View members */}
 						<View style={[styles.memberInfo, styles.myBorder]}>
 							<View
-								style={[
-									styles.bodyHeader,
-									{ height: 35, marginBottom: 16 },
-								]}
-							>
+								style={[styles.bodyHeader, { height: 35, marginBottom: 16 }]}>
 								<Text>Thông tin người ở</Text>
 
 								<TouchableOpacity
@@ -337,12 +281,8 @@ export default function App({ navigation, route }) {
 										navigation.navigate('AddMember', {
 											roomID: room.id,
 										});
-									}}
-								>
-									<FontAwesomeIcon
-										name="plus-circle"
-										size={30}
-									/>
+									}}>
+									<FontAwesomeIcon name="plus-circle" size={30} />
 								</TouchableOpacity>
 							</View>
 
@@ -352,79 +292,51 @@ export default function App({ navigation, route }) {
 									nestedScrollEnabled={true}
 									data={memberList}
 									renderItem={renderMembers}
-									keyExtractor={(item) => item.id}
-								></FlatList>
+									keyExtractor={(item) => item.id}></FlatList>
 							</View>
 						</View>
 
 						{/*View service */}
 						<View style={[styles.serviceInfo, styles.myBorder]}>
 							<View
-								style={[
-									styles.bodyHeader,
-									{ height: 35, marginBottom: 16 },
-								]}
-							>
+								style={[styles.bodyHeader, { height: 35, marginBottom: 16 }]}>
 								<Text>Thông tin dịch vụ</Text>
 								<TouchableOpacity
 									onPress={() => {
 										navigation.navigate('EditService', {
-											roomID: room.id
-										})
-									}}
-								>
-									<FontAwesomeIcon
-										name="edit"
-										size={25}
-									/>
+											roomID: room.id,
+										});
+									}}>
+									<FontAwesomeIcon name="edit" size={25} />
 								</TouchableOpacity>
 							</View>
 							<View style={[styles.serviceContainer]}>
 								{/* Water */}
-								<View
-									style={[
-										styles.service,
-										styles.myBackground,
-									]}
-								>
+								<View style={[styles.service, styles.myBackground]}>
 									<View
 										style={{
 											flexDirection: 'row',
 											alignItems: 'center',
-										}}
-									>
+										}}>
 										<IonIcon
 											name="water"
 											size={20}
 											style={{ marginRight: 20 }}
 										/>
-										<Text>
-											{waterFee} đ/khối
-										</Text>
+										<Text>{waterFee} đ/khối</Text>
 									</View>
-									<View
-										style={[
-											styles.lastestUnit,
-											styles.myBorder,
-										]}
-									>
+									<View style={[styles.lastestUnit, styles.myBorder]}>
 										<Text>{room.old_water_number}</Text>
 									</View>
 								</View>
 
 								{/* Electricity */}
-								<View
-									style={[
-										styles.service,
-										styles.myBackground,
-									]}
-								>
+								<View style={[styles.service, styles.myBackground]}>
 									<View
 										style={{
 											flexDirection: 'row',
 											alignItems: 'center',
-										}}
-									>
+										}}>
 										<FontAwesomeIcon
 											name="bolt"
 											size={20}
@@ -435,59 +347,47 @@ export default function App({ navigation, route }) {
 										/>
 										<Text>{electricityFee} đ/kwh</Text>
 									</View>
-									<View
-										style={[
-											styles.lastestUnit,
-											styles.myBorder,
-										]}
-									>
+									<View style={[styles.lastestUnit, styles.myBorder]}>
 										<Text>{room.old_electricity_number}</Text>
 									</View>
 								</View>
 
 								{/* Wifi */}
-								<View
-									style={[
-										styles.service,
-										styles.myBackground,
-									]}
-								>
+								<View style={[styles.service, styles.myBackground]}>
 									<View
 										style={{
 											flexDirection: 'row',
 											alignItems: 'center',
-										}}
-									>
-										<IonIcon name="wifi" size={20}  style={{
+										}}>
+										<IonIcon
+											name="wifi"
+											size={20}
+											style={{
 												marginRight: 20,
-											}}/>
-										
+											}}
+										/>
+
 										<Text>Tiền wifi</Text>
 									</View>
-									
-									
 								</View>
 
 								{/* Tiền rác */}
-								<View
-									style={[
-										styles.service,
-										styles.myBackground,
-									]}
-								>
+								<View style={[styles.service, styles.myBackground]}>
 									<View
 										style={{
 											flexDirection: 'row',
 											alignItems: 'center',
-										}}
-									>
-										<IonIcon name="trash" size={20}  style={{
+										}}>
+										<IonIcon
+											name="trash"
+											size={20}
+											style={{
 												marginRight: 20,
-											}}/>
-										
+											}}
+										/>
+
 										<Text>Tiền rác</Text>
 									</View>
-
 								</View>
 							</View>
 						</View>
@@ -497,14 +397,10 @@ export default function App({ navigation, route }) {
 							style={styles.deleteButton}
 							onPress={() => {
 								// Handle reset room information
-							}}
-						>
-							<Text style={styles.textTitleWhite}>
-								XÓA THÔNG TIN PHÒNG
-							</Text>
+							}}>
+							<Text style={styles.textTitleWhite}>XÓA THÔNG TIN PHÒNG</Text>
 						</TouchableOpacity>
 					</View>
-					
 				)}
 
 				{/* View Bill */}
@@ -524,8 +420,7 @@ export default function App({ navigation, route }) {
 									style={{
 										color: 'white',
 										fontWeight: 'bold',
-									}}
-								>
+									}}>
 									{totalRemained}
 								</Text>
 							</View>
@@ -535,12 +430,9 @@ export default function App({ navigation, route }) {
 							nestedScrollEnabled={true}
 							data={billList}
 							renderItem={renderBills}
-							keyExtractor={(item) => item.id}
-						></FlatList>
+							keyExtractor={(item) => item.id}></FlatList>
 					</View>
 				)}
-
-
 			</ScrollView>
 		</View>
 	);
@@ -619,7 +511,7 @@ const styles = StyleSheet.create({
 	memberContainer: {
 		width: '100%',
 		height: '85%',
-		flex: 1
+		flex: 1,
 	},
 	member: {
 		width: '100%',
@@ -659,13 +551,13 @@ const styles = StyleSheet.create({
 	deleteIcon: {
 		position: 'absolute',
 		right: 0,
-		backgroundColor: 'black', 
-		height: '100%', 
+		backgroundColor: 'black',
+		height: '100%',
 		width: 30,
-		justifyContent: 'center', 
+		justifyContent: 'center',
 		alignItems: 'center',
-		borderTopRightRadius: 10, 
-		borderBottomRightRadius: 10
+		borderTopRightRadius: 10,
+		borderBottomRightRadius: 10,
 	},
 	deleteButton: {
 		backgroundColor: 'black',
@@ -673,7 +565,7 @@ const styles = StyleSheet.create({
 		height: '5%',
 		borderRadius: 10,
 		marginBottom: 12,
-		justifyContent: 'center'
+		justifyContent: 'center',
 	},
 
 	billContainer: {
@@ -684,7 +576,7 @@ const styles = StyleSheet.create({
 	billInfoContainer: {
 		marginBottom: 32,
 		flexDirection: 'row',
-		alignItems: 'center'
+		alignItems: 'center',
 	},
 	billInfo: {
 		height: 100,
