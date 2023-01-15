1f390c1c537d63635a30c999e74234e762f12fd1
daotrananhtuan09102002 <79463278+daotrananhtuan09102002@users.noreply.github.com>
2023-01-15 15:02:27 +0700

    update UI addnewroom

--- DoAn/RentHouse/src/StackComponents/AddNewRoom.js
+++ DoAn/RentHouse/src/StackComponents/AddNewRoom.js
@@ -5,18 +5,41 @@ import {
     TextInput,
     TouchableOpacity,
 } from 'react-native';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import IonIcon from 'react-native-vector-icons/Ionicons';
 
 import { useForceUpdate } from "../utils/utils";
 import { fetchRoomList, insertRoom } from "../database/actions/roomActions";
+import { fetchServiceDetails } from '../database/actions/serviceActions';
 
 export default function App({ navigation, route }) {
     const [roomName, setRoomName] = useState('');
     const [price, setPrice] = useState('');
     const [electricityCurrent, setElectricityCurrent] = useState('');
     const [waterServiceCurrent, setWaterServiceCurrent] = useState('');
+
+    const [electricityPrice, setElectricityPrice] = useState(null);
+    const [waterPrice, setWaterPrice] = useState(null);
+    const [garbagePrice, setGarbagePrice] = useState(null);
+
+    // Load service price from database
+    useEffect(() => {
+
+        const loadServicePrices = async (service_name, setServicePrice) => {
+            const servicePrices = await fetchServiceDetails(service_name)
+                .catch((error) => console.log(error));
+
+            setServicePrice(String(servicePrices['price']));
+        };
+
+        loadServicePrices('Điện', setElectricityPrice);
+        loadServicePrices('Nước', setWaterPrice);
+        loadServicePrices('Rác', setGarbagePrice);
+
+    }, []);
 
     const handleAddRoom = () => {
         insertRoom(
@@ -56,7 +79,10 @@ export default function App({ navigation, route }) {
             </View>
 
             <View style={styles.serviceCurrent}>
-                <Text style={styles.text}>Chỉ số điện nước hiện tại</Text>
+                <Text style={[styles.text, {borderBottomColor:'black', borderBottomWidth:1, 
+                width:'100%', paddingBottom:8, paddingHorizontal:10}]}>
+                    Chỉ số điện nước hiện tại
+                </Text>
 
                 <View style={styles.inputService}>
                     <Text style={styles.text}>Chỉ số điện</Text>
@@ -83,8 +109,24 @@ export default function App({ navigation, route }) {
                 </View>
             </View>
 
-            <View style={styles.Service}>
-                <Text>Dịch vụ</Text>
+            <View style={styles.ServiceContainer}>
+                <Text style={[styles.text, {borderBottomColor:'black', borderBottomWidth:1, 
+                width:'100%', paddingBottom:8, paddingHorizontal:10}]}>Dịch vụ</Text>
+                <View style={styles.service}>
+                    <FontAwesomeIcon name="bolt" size={30} style={{ paddingHorizontal: 8, paddingVertical: 4 }} />
+                    <Text style={styles.text}>Điện</Text>
+                    <Text style={styles.text}>{electricityPrice} đ/kWh</Text>
+                </View>
+                <View style={[styles.service]}>
+                    <IonIcon name="water" size={30} style={{paddingVertical: 2 }} />
+                    <Text style={[styles.text]}>Nước</Text>
+                    <Text style={styles.text}>{waterPrice} đ/m3</Text>
+                </View>
+                <View style={styles.service}>
+                    <IonIcon name="trash" size={25} style={{ paddingHorizontal: 2, paddingVertical: 4 }} />
+                    <Text style={styles.text}>Rác</Text>
+                    <Text style={styles.text}>{garbagePrice} đ/tháng</Text>
+                </View>
                 
             </View>
 
@@ -139,7 +181,7 @@ styles = StyleSheet.create({
         borderColor: 'black',
         borderWidth: 1,
         borderRadius: 10,
-        padding: 10,
+        paddingTop: 10,
         marginTop: 10,
     },
 
@@ -161,6 +203,25 @@ styles = StyleSheet.create({
         position: 'absolute',
         bottom: 10,
     },
+
+    ServiceContainer: {
+        width: '95%',
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 10,
+        paddingTop:10,
+        marginTop: 10,
+        
+    },
+
+    service: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: '95%',
+        padding: 10,
+    },
+
 
 });
 
